ed142f93ac85b5c684d6354d3548ba3db4d189a8
Carlos Garro <carlosgarro11@gmail.com>
2026-09-11 19:44:16 -0400

    Adding pytest fixture and delete test

--- tests/test_applications.py
+++ tests/test_applications.py
@@ -1,9 +1,19 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from app import main
+import pytest
 
-client = TestClient(app)
+client = TestClient(main.app)
 
-def test_get_applications():
+@pytest.fixture
+def reset_apps():
+    main.applications[:] = [
+        {"id": 1, "company": "Company A", "position": "Software Dev", "status": "Applied"},
+        {"id": 2, "company": "Company B", "position": "Data Analyst", "status": "Interview Scheduled"},
+        {"id": 3, "company": "Company C", "position": "Project Manager", "status": "Offer Received"}
+    ]
+    yield
+
+def test_get_applications(reset_apps):
     response = client.get("/applications/1")
     data = response.json()
     application = data["application"]
@@ -12,12 +22,12 @@ def test_get_applications():
     assert application["company"] == "Company A"
     assert application["status"] == "Applied"
 
-def test_get_application_not_found():
+def test_get_application_not_found(reset_apps):
     response = client.get("/applications/99")
     assert response.status_code == 404
     assert response.json() == {"detail": "Application not found"}
 
-def test_create_application():
+def test_create_application(reset_apps):
     new_application = {
         "company" : "Apple",
         "position" : "Engineer",
@@ -32,3 +42,22 @@ def test_create_application():
     assert application["position"] == new_application["position"]
     assert application["status"] == new_application["status"]
     assert isinstance(application["id"], int)
+
+def test_update_application(reset_apps):
+    update = {
+        "status": "Offer Given"
+    }
+
+    response = client.patch("/applications/1", json=update)
+    data = response.json()
+    application  = data["application"]
+    assert response.status_code == 200
+    assert application["company"] == "Company A"
+    assert application["position"] == "Software Dev"
+    assert application["status"] == "Offer Given"
+
+def test_delete_application(reset_apps):
+    response = client.delete("/applications/3")
+    assert response.status_code == 200
+    response = client.get("/applications/3")
+    assert response.status_code == 404
